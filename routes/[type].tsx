4499fc80951377fa5bd932d3ea0241041228d241
Ibrahim Odev <ibrahimodev42@gmail.com>
2023-06-29 11:28:31 +0300

    ui

--- routes/[type].tsx
+++ routes/[type].tsx
@@ -17,18 +17,20 @@ export default function Type(props: TypeProps) {
       <Head>
         <title>Score App</title>
       </Head>
-      <main class="container mx-auto">
-        <h1 class="text-4xl font-bold my-4 mx-2 uppercase">
-          {props.params.type}
-        </h1>
-        <ScoreArea
-          players={players}
-          scoreType={
-            props.params.type === "reduction"
-              ? ScoreType.REDUCTION
-              : ScoreType.EQUALIZATION
-          }
-        />
+      <main class="container">
+        <div class="text-center flex items-center justify-center flex-col">
+          <h1 class="text-4xl font-bold my-4 mx-2 uppercase">
+            {props.params.type}
+          </h1>
+          <ScoreArea
+            players={players}
+            scoreType={
+              props.params.type === "reduction"
+                ? ScoreType.REDUCTION
+                : ScoreType.EQUALIZATION
+            }
+          />
+        </div>
       </main>
     </>
   );
--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Score App</title>
       </Head>
-      <main class="container mx-auto">
+      <main class="container">
         <a href="/equalization" class="underline text-indigo-500 hover:text-indigo-600 p-2">equalization</a>
         <a href="/reduction" class="underline text-indigo-500 hover:text-indigo-600 p-2">reduction</a>
       </main>
